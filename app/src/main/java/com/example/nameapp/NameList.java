6e736dfb247ee66de6b80cb026a974218523696f
theaoen <thea.oen@hotmail.com>
2017-01-17 17:58:39 +0100

    Første versjon med liste med namn

--- app/src/main/java/com/example/nameapp/NameList.java
+++ app/src/main/java/com/example/nameapp/NameList.java
@@ -1,8 +1,15 @@
 package com.example.nameapp;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NameList extends AppCompatActivity {
 
@@ -10,6 +17,41 @@ public class NameList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
+
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        String[] values = {"Alpha", "Beta", "Gamma", "Delta"
+                , "Epsilon", "Zeta", "Eta", "Theta"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
 
     }
 }
