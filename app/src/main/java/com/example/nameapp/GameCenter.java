cc6c2d56023dc5e7258520a047a74b1b32a4b14b
theaoen <thea.oen@hotmail.com>
2017-01-19 10:57:25 +0100

    Delar av "spelet" skal funke

---
+++ app/src/main/java/com/example/nameapp/GameCenter.java
@@ -0,0 +1,82 @@
+package com.example.nameapp;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.example.nameapp.Person;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.example.nameapp.PersonList.getListe;
+import static com.example.nameapp.PersonList.initialize;
+import static com.example.nameapp.PersonList.listInitialized;
+
+/**
+ * Created by theaoen on 19.01.2017.
+ */
+
+public class GameCenter {
+
+    Context context;
+    Integer score;
+    Integer attempts;
+    Integer randomNumber;
+    Person p;
+    String correctName;
+    Uri imgUri;
+    final ArrayList<Person> liste;
+
+    //Konstruktør
+    public GameCenter(Context c){
+        context = c;
+        score = 0;
+        attempts = 0;
+
+        if(!listInitialized()){
+            initialize(context);
+        }
+
+        liste = getListe();
+
+    }
+
+    //Hentar inn neste bilete
+    public void nextPicture(){
+        //Lagar tilfeldig tal for å hente eit gitt bilete
+        Random rand = new Random(System.currentTimeMillis());
+        randomNumber = rand.nextInt(liste.size());
+
+
+        //Hentar eit nytt bilete, og finn namn og Uri.
+        p = liste.get(randomNumber);
+        correctName = p.name;
+        imgUri = p.uri;
+
+    }
+
+    //Sjekkar om svaret var riktig
+    public boolean checkAnswer(String name){
+        if(correctName.toLowerCase().equals(name.toLowerCase())){
+            attempts++;
+            score++;
+            return true;
+        }
+        else{
+            attempts++;
+            return false;
+        }
+    }
+
+
+    //Metode som genererar tilfeldig tal i intervall
+    private Integer createRandom(Integer min, Integer max){
+        Random rand = new Random();
+        return min + rand.nextInt((max - min) + 1);
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/nameapp/LearningMode.java
+++ app/src/main/java/com/example/nameapp/LearningMode.java
@@ -1,13 +1,58 @@
 package com.example.nameapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class LearningMode extends AppCompatActivity {
+
+    GameCenter gameCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_mode);
+
+        gameCenter = new GameCenter(this);
+        gameCenter.nextPicture();
+
+        updatePicture();
+
+    }
+
+    public void sendAnswer(View view) {
+       // Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        editText.setText("");
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+
+        if(gameCenter.checkAnswer(message)){
+            Toast.makeText(LearningMode.this, "Riktig!" + gameCenter.randomNumber, Toast.LENGTH_SHORT).show();
+
+
+            gameCenter.nextPicture();
+            updatePicture();
+        }
+        else{
+
+            Toast.makeText(LearningMode.this, "Feil! Namnet var " + gameCenter.correctName + gameCenter.randomNumber, Toast.LENGTH_SHORT).show();
+
+            gameCenter.nextPicture();
+            updatePicture();
+        }
+
+    }
+
+    public void updatePicture(){
+        final ImageView imgview = (ImageView) findViewById(R.id.imageView);
+        imgview.setImageURI(gameCenter.imgUri);
     }
 }
