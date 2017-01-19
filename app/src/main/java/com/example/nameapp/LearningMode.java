00c83de20098b2ad080f2b590f994a5bd02050c8
theaoen <thea.oen@hotmail.com>
2017-01-19 12:11:24 +0100

    Lagt på kommentarar

--- app/src/main/java/com/example/nameapp/LearningMode.java
+++ app/src/main/java/com/example/nameapp/LearningMode.java
@@ -17,39 +17,41 @@ public class LearningMode extends AppCompatActivity {
 
     GameCenter gameCenter;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_mode);
 
-
-
         gameCenter = new GameCenter(this);
         gameCenter.nextPicture();
-
         updatePicture();
         updateScore();
 
     }
 
     public void sendAnswer(View view) {
+        //Hentar inn svaret frå brukaren og nullar ut tekstfeltet
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         editText.setText("");
 
+        //Brukar spel-klassen til å sjekke svar og lage klart nytt bilete.
         gameCenter.checkAnswer(message);
         gameCenter.nextPicture();
+
+        //Oppdaterar view for bilete og tekst.
         updatePicture();
         updateScore();
 
     }
 
+    //Metode som legg inn bilete i imageView
     public void updatePicture(){
         final ImageView imgview = (ImageView) findViewById(R.id.imageView);
         imgview.setImageURI(gameCenter.imgUri);
     }
 
+    //Metode som oppdaterar tekstfelt med score og tal forsøk.
     public void updateScore(){
 
         String numCorrect = "Score: " + gameCenter.score;
