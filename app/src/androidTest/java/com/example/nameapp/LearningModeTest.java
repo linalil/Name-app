656f07870365ab28f650c2d152c1da48a9d4fd8a
Lina Lillejordet <lina.lillejordet@gmail.com>
2017-01-31 13:12:41 +0100

    LearningModeTest

---
+++ app/src/androidTest/java/com/example/nameapp/LearningModeTest.java
@@ -0,0 +1,36 @@
+package com.example.nameapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by linalil on 31.01.2017.
+ */
+public class LearningModeTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void sendAnswer() throws Exception {
+
+    }
+
+    @Test
+    public void updatePicture() throws Exception {
+
+    }
+
+    @Test
+    public void updateScore() throws Exception {
+
+    }
+
+    @Test
+    public void goBack() throws Exception {
+
+    }
+
+}
