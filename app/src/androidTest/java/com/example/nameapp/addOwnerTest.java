969017222e8ac960dfd7942b1477cf1c70140b31
theaoen <thea.oen@hotmail.com>
2017-02-01 22:12:36 +0100

    Sletta midlertidig testplan

--- app/src/androidTest/java/com/example/nameapp/addOwnerTest.java
+++ app/src/androidTest/java/com/example/nameapp/addOwnerTest.java
@@ -1,31 +1,47 @@
 package com.example.nameapp;
 
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
+import android.util.Log;
+
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Collection;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
  * Created by linalil on 31.01.2017.
  */
 public class addOwnerTest {
-    @Test
-    public void onCreate() throws Exception {
 
+
+    @Rule
+    public ActivityTestRule<addOwner> mActivityRule = new ActivityTestRule<>(addOwner.class);
+
+    /*
+    * Tester om det står "Type your name:" i tekstboksen med id assNameText
+    * */
+    @Test
+    public void checkForNameText() throws Exception {
+        onView(withId(R.id.addNameText)).check(matches(withText("Type your name:")));
     }
 
+    /*
+    * Legger inn teksten "Per" i textView med id edit_name og sjekker om denne teksten faktisk finnes i textViewet
+    */
     @Test
-    public void browse_gallery() throws Exception {
-
-    }
-
-    @Test
-    public void onActivityResult() throws Exception {
-
-    }
-
-    @Test
-    public void addPersonToApp() throws Exception {
-
+    public void validateEditText() {
+        onView(withId(R.id.edit_owner)).perform(typeText("Per")).check(matches(withText("Per")));
     }
 
 }
